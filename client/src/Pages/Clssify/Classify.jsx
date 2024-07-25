67b777655423cafea692a6bd23752f094c64589e
mxnattha <mx.nattha963@gmail.com>
2024-07-25 21:37:03 +0700

    initial commit

---
+++ client/src/Pages/Clssify/Classify.jsx
@@ -0,0 +1,217 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Nav from '../../Components/Navbar/Nav';
+import ProgressBar from '../../Components/ProgressBar/ProgressBar';
+import './Classify.css'
+import upload from '../../assets/upload.png'
+import barchart from '../../assets/bar-chart.png'
+import { Oval } from 'react-loader-spinner'
+
+function Classify() {
+    const [image, setImage] = useState(null);
+    const [imageUrl, setImageUrl] = useState(null);
+    const [prediction, setPrediction] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [model, setModel] = useState('');
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [placeHolder, setPlaceHolder] = useState('Select Model');
+    const [modelData, setModelData] = useState({ top1Accuracy: '-', top5Accuracy: '-', Parameters: '-' });
+
+    const handleImageUpload = (event) => {
+        console.log('this function triggerd')
+        const file = event.target.files[0];
+        if (file) {
+            setImage(file);
+            setImageUrl(URL.createObjectURL(file));
+
+            // Convert selected file to base64
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = () => {
+                const base64Data = reader.result.split(',')[1];
+                setImage(base64Data); // Set the image state to base64 encoded string
+            };
+        }
+    };
+
+    const handleDisplayClick = () => {
+        document.getElementById('fileInput').click();
+    };
+
+    const handlePrediction = async () => {
+        setPrediction(null);
+        if (!image) {
+            alert('Please upload an image first.')
+            return;
+        }
+
+        setLoading(true);
+
+        try {
+        
+            
+            // console.log(image) // image in this state is base64
+            const response = await axios.post('http://localhost:5000/cls_upload-image', { image, model });
+            console.log(response.data);
+            setPrediction(response.data);
+
+        } catch (error) {
+            console.error('Error predicting image:', error);
+            alert('Failed to predict image. Please try again.');
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const handleNewImageUpload = () => {
+        // Clear the image state and URL
+        setImage(null);
+        setImageUrl(null);
+        setLoading(false)
+        // Trigger the file input click
+        handleDisplayClick();
+    };
+
+    const handleModelSelect = (selectedModel) => {
+        setModel(selectedModel);
+        setPlaceHolder(selectedModel);
+        setModelData(models[selectedModel]);
+        setDropdownOpen(false);
+    }
+
+    const models = {
+        'Efficientnet B0': { top1Accuracy: '77.7%', top5Accuracy: '93.5%', Parameters: '5.3M' },
+        'Efficientnet B1': { top1Accuracy: '78.6%', top5Accuracy: '94.2%', Parameters: '7.8M' },
+        'Efficientnet B2': { top1Accuracy: '80.6%', top5Accuracy: '95.3%', Parameters: '9.1M' },
+        'Efficientnet B3': { top1Accuracy: '82.0%', top5Accuracy: '96.0%', Parameters: '12.2M' }
+    };
+
+    return(<div>
+                <Nav />
+                
+                <div className='classification-features'>
+
+
+                    {/* section 1 */}
+                    <div className='dashboard-body'>
+                        <div className='control-panel'>
+                            <div className='model-header'>
+                                <h2>Classification Model</h2>
+                            </div>
+                            <div className='model-dropdown-box'>
+                                
+                                <input 
+                                    type="text" 
+                                    className='textBox' 
+                                    placeholder={placeHolder}
+                                    readonly
+                                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                                />
+
+                                
+                                {dropdownOpen && (
+                                <div className='model-option'>
+                                    {Object.keys(models).map((modelKey) => (
+                                        <div key={modelKey} onClick={() => handleModelSelect(modelKey)}>
+                                            {modelKey}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                            </div>
+                            {!image && <button className='upload-button' onClick={handleDisplayClick}>Upload image</button>}
+                            {image && <button className='upload-button' onClick={handleNewImageUpload}>Upload new image</button>}
+                            {image && <button className='predict-button' onClick={handlePrediction}>Let's Predict</button>}
+                            
+                        </div>
+
+                        
+                        <div className='dashboard-container'>
+                            {Object.entries(modelData).map(([key, value], index) => (
+                                <div className='dashboard-card' key={index}>
+                                    <div className='dashboard-value'>{value}</div>
+                                    <div className='dashboard-title'>{key.replace(/([A-Z])/g, ' $1')}</div>
+                                </div>
+                            ))}
+                        </div>
+                        
+                    </div>
+
+
+                    {/* section 2 */}
+                    <div className='prediction-body'>
+
+                        <div className='upload-box'>
+                            <div className='upload-container'>
+                                {imageUrl ? null : (<img src={upload} alt="upload-logo" className='upload-logo'/>)}
+                                
+                                {imageUrl ? (<img src={imageUrl} alt="uploaded-img" className='uploaded-img'/>) : 
+                                            (<p> upload your image</p>)}
+                            </div>
+
+                            <input 
+                                type="file" 
+                                id='fileInput'
+                                style={{display: 'none'}}
+                                onChange = {handleImageUpload}
+                            />
+                        
+                
+                        </div>
+
+                        {/* <div className='buttons-box'>
+                            <div className='buttons'>
+                                {!image && <button className='upload-button' onClick={handleDisplayClick}>Upload new image</button>}
+                                {image && <button className='upload-button' onClick={handleNewImageUpload}>Upload new image</button>}
+                                {image && <button className='predict-button' onClick={handlePrediction}>Let's Predict</button>}
+                            </div>
+                        </div> */}
+
+
+
+                        <div className='prediction-box'>
+                                {prediction ? (
+                                <div>
+                                    <ProgressBar
+                                    label1={prediction.Class1}
+                                    value1={prediction.Prob1}
+                                    label2={prediction.Class2}
+                                    value2={prediction.Prob2}
+                                    label3={prediction.Class3}
+                                    value3={prediction.Prob3}
+                                    label4={prediction.Class4}
+                                    value4={prediction.Prob4}
+                                    />
+                                </div>
+                                ) : (
+                                <div className='unpredict'>
+                                    {loading ? (
+                                    <Oval
+                                        height={80}
+                                        width={80}
+                                        color="#5A639C"
+                                        wrapperStyle={{}}
+                                        wrapperClass=""
+                                        visible={true}
+                                        ariaLabel='oval-loading'
+                                        secondaryColor="#9B86BD"
+                                        strokeWidth={2}
+                                        strokeWidthSecondary={2}
+                                    />
+                                    ) : (
+                                    <>
+                                        <img src={barchart} alt="upload-logo" className='upload-logo' />
+                                        <p>prediction</p>
+                                    </>
+                                    )}
+                                </div>
+)}
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>);
+}
+
+export default Classify;
---
+++ web-server/server.js
@@ -0,0 +1,157 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import axios from 'axios';
+import pg from "pg";
+import bcrypt from "bcrypt";
+import env from "dotenv";
+
+
+const app = express();
+const port = 5000;
+const saltRounds = 10;
+
+env.config();
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+});
+
+db.connect()
+
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extend: true}));
+
+app.get("/", (req, res) => {
+    res.json({message: 'Hello from the server'})
+})
+
+// login endpoint
+app.post("/login", async (req, res) => {
+    // console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const result = await db.query("SELECT * FROM users WHERE email = $1", [email])
+        if (result.rows.length > 0) {
+            const user = result.rows[0];
+            const storedHashedPassword = user.password;
+
+            //verifying the password
+            bcrypt.compare(password, storedHashedPassword, (err, result) => {
+                if (err) {
+                    console.log("Error comparing passwords:", err);
+                } else {
+                    if (result) {
+                        res.json({ message: 'Login successful' });
+                    } else {
+                        res.json({ message: 'Incorrect password' });
+                    }
+                }
+            })
+        } else {
+            res.json({ message: 'User not found' })
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+// register endpoint
+app.post("/register", async (req, res) => {
+    
+    const regEmail = req.body.email;
+    const regPassword = req.body.password;
+    try {
+        const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [regEmail])
+
+        if (checkResult.rows.length > 0) {
+            res.json({message: 'Email already exists. Try logging in.'});
+        } else {
+            bcrypt.hash(regPassword, saltRounds, async (err, hash) => {
+                if (err) {
+                    console.error("Error hashing password:", err);
+                } else {
+                    const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",
+                        [regEmail, hash]
+                    );
+                    res.json({message: 'Your registration already successfully'});
+                }
+            })
+        }    
+    } catch (err) {
+        console.log(err);
+    }
+    console.log(regEmail);
+    console.log(regPassword);
+})
+
+// classify endpoint
+app.post("/cls_upload-image", async (req, res) => {
+    console.log('image has send to this endpoint');
+    console.log(req.body);
+    // res.json({message: 'predict successful'})
+    try {
+        const image = req.body.image;
+        const model = req.body.model;
+        console.log(model)
+        // Forward image to aiserver.py
+        const aiserverURL = 'http://localhost:3000/cls_predict'
+        const response = await axios.post(aiserverURL, {image, model});
+
+        // Return response from aiserver.py to the client
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error forwarding image to aiserver:', error);
+        res.status(500).json({ error: 'Failed to forward image to aiserver' });
+    }
+})
+
+// object detection endpoint
+app.post("/obj_upload-image", async (req, res) => {
+    console.log('image has send to this endpoint');
+    console.log(req.body);
+    // res.json({message: 'predict successful'})
+    try {
+        const {image} = req.body;
+        // Forward image to aiserver.py
+        const aiserverURL = 'http://localhost:3000/obj_predict'
+        const response = await axios.post(aiserverURL, {image});
+
+        // Return response from aiserver.py to the client
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error forwarding image to aiserver:', error);
+        res.status(500).json({ error: 'Failed to forward image to aiserver' });
+    }
+})
+
+// segment endpoint
+app.post("/seg_upload-image", async (req, res) => {
+    console.log('image has send to this endpoint');
+    console.log(req.body);
+    // res.json({message: 'predict successful'})
+    try {
+        const {image} = req.body;
+        // Forward image to aiserver.py
+        const aiserverURL = 'http://localhost:3000/seg_predict'
+        const response = await axios.post(aiserverURL, {image});
+
+        // Return response from aiserver.py to the client
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error forwarding image to aiserver:', error);
+        res.status(500).json({ error: 'Failed to forward image to aiserver' });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
+  });
